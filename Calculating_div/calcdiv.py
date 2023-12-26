98e393249148a291725ad54913c1638dfd04ee91
nataliemallen <86844435+nataliemallen@users.noreply.github.com>
2023-12-26 15:50:04 -0600

    Update calcdiv.py

--- Calculating_div/calcdiv.py
+++ Calculating_div/calcdiv.py
@@ -1,10 +1,13 @@
 import csv
 import math
+import os
 
-def calculate_k2p_distance(var_file, output_file):
-    # Initialize counts for transitions and transversions
+def calculate_distances(var_file, paf_file):
+    # Initialize counts for transitions, transversions, and total differences
     transition_count = 0
     transversion_count = 0
+    total_differences = 0
+    total_sites = 0
 
     # Define sets for transition and transversion bases
     transitions = {"ag", "ga", "ct", "tc"}
@@ -12,51 +15,54 @@ def calculate_k2p_distance(var_file, output_file):
 
     with open(var_file, 'r') as file:
         for line in file:
-            # Skip lines that do not start with 'V'
             if not line.startswith('V'):
                 continue
 
-            # Split the line into columns
             columns = line.strip().split()
 
-            # Skip lines where column 5 is not one
             if columns[4] != '1':
                 continue
 
-            # Get the REF and ALT alleles
             ref_allele = columns[6].lower()
             alt_allele = columns[7].lower()
 
-            # Skip lines where REF or ALT is not a single base
             if len(ref_allele) != 1 or len(alt_allele) != 1:
                 continue
 
-            # Check if the change is a transition or transversion
             change = ref_allele + alt_allele
             if change in transitions:
                 transition_count += 1
             elif change in transversions:
                 transversion_count += 1
 
-    # Calculate the proportions of transitions and transversions
-    p = transition_count / (transition_count + transversion_count)
-    q = transversion_count / (transition_count + transversion_count)
+            total_differences += 1
 
-    # Print the counts and proportions for debugging
-    print(f'Transitions: {transition_count}, Transversions: {transversion_count}, p: {p}, q: {q}')
+    with open(paf_file, 'r') as file:
+        for line in file:
+            columns = line.strip().split()
+            start = int(columns[7])
+            end = int(columns[8])
+            total_sites += end - start
 
-    # Check if the argument of the math.log function is greater than zero
-    if (1 - 2*p - q) * math.sqrt(1 - 2*q) > 0:
-        # Calculate the K2P distance
-        k2p_distance = -0.5 * math.log((1 - 2*p - q) * math.sqrt(1 - 2*q))
-    else:
-        k2p_distance = 'undefined'
+    # Calculate the proportions of transitions, transversions, and total differences
+    p = transition_count / total_sites
+    q = transversion_count / total_sites
+    r = total_differences / total_sites
 
-    # Write the K2P distance to the output CSV file
+    # Calculate the distances
+    raw_distance = r
+    k2p_distance = (-0.5 * math.log(1 - (2 * p - q))) - (0.25 * math.log(1 - (2 * q)))
+    k3p_distance = (-0.5 * math.log(1 - (2 * p - q))) - (0.20 * math.log(1 - (2 * q)))
+    jc_distance = -0.75 * math.log(1 - (4/3) * r)
+
+    # Write the distances to the output CSV file
+    output_file = os.path.splitext(var_file)[0] + '_distance.csv'
     with open(output_file, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['K2P Distance'])
-        writer.writerow([k2p_distance])
+        writer.writerow(['Raw Distance', raw_distance])
+        writer.writerow(['K2P Distance', k2p_distance])
+        writer.writerow(['K3P Distance', k3p_distance])
+        writer.writerow(['JC Distance', jc_distance])
 
 # Call the function with your input and output file paths
-calculate_k2p_distance('falcons.var.txt', 'output.csv')
+calculate_distances('falcons.var.txt', 'falcons.srt.paf')
