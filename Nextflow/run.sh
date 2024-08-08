811c6c0b98d089b7506e9d024f681a4141259468
nataliemallen <86844435+nataliemallen@users.noreply.github.com>
2024-08-08 16:44:16 -0400

    Update run.sh

--- Nextflow/run.sh
+++ Nextflow/run.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+#SBATCH -A fnrdewoody
+#SBATCH -t 14-00:00:00
+#SBATCH --job-name=nextflow_job
+#SBATCH --output=nextflow_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=allen715@purdue.edu
+#SBATCH --mail-type=END,FAIL
+
+module load nextflow
+
+nextflow run main.nf -c nextflow.config -resume
 
